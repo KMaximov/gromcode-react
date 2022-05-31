abc62d7d81fe8dd082c49d9ca10ea12643d228af
Mykola <k.maximov19@gmail.com>
2022-05-31 15:06:03 +0300

    docs: complete lesson3 task5

---
+++ lesson3/task5/src/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import moment from 'moment';
+
+const Profile = props => {
+  return <>
+    <p className="profile__name">{props.userData.firstName} {props.userData.lastName}</p>
+    <p className="profile__birth">Was born {moment(props.userData.birthDate).format("DD MMM YY")} in {props.userData.birthPlace}</p>
+  </>
+}
+
+export default Profile;
