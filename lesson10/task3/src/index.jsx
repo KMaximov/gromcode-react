5bfb7b4d4d8942e7fa7e842f0d643cca3beb8eeb
Mykola <k.maximov19@gmail.com>
2022-06-14 22:28:35 +0300

    docs: complete lesson10 task3-4

--- lesson10/task3/src/index.jsx
+++ lesson10/task3/src/index.jsx
@@ -8,7 +8,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import Calculator from './Calculator';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render( rootElement);
+ReactDOM.render(<Calculator />, rootElement);
