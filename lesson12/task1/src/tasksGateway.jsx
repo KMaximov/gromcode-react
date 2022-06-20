9bc4bb664df85b41d6c047d709f71e1b4d1b391c
Mykola <k.maximov19@gmail.com>
2022-06-20 17:24:50 +0300

    docs: complete lesson12 task1

---
+++ lesson12/task1/src/tasksGateway.jsx
@@ -0,0 +1,45 @@
+const baseUrl = 'https://627a4e1373bad50685866f2c.mockapi.io/api/v1/TodoList';
+
+export const createTask = taskData =>
+  fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(taskData),
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to create task');
+    }
+  });
+
+export const fetchTasksList = () =>
+  fetch(baseUrl)
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(tasksList => tasksList.map(({ _id, ...task }) => ({ id: _id, ...task })));
+
+export const updateTask = (taskId, taskData) =>
+  fetch(`${baseUrl}/${taskId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(taskData),
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to create task');
+    }
+  });
+
+export const deleteTask = taskId =>
+  fetch(`${baseUrl}/${taskId}`, {
+    method: 'DELETE',
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to create task');
+    }
+  });
