2877e586cb9b922d4b09fa252ad5deb6ce845ed2
Mykola <k.maximov19@gmail.com>
2022-06-20 17:31:59 +0300

    docs: complete lesson12 task1

--- lesson12/task1/src/Task.jsx
+++ lesson12/task1/src/Task.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
 const Task = ({ id, done, text, onChange, onDelete }) => {
   const listItemClasses = classNames('list-item', { 'list-item_done': done });
@@ -15,6 +16,14 @@ const Task = ({ id, done, text, onChange, onDelete }) => {
       <button className="list-item__delete-btn" onClick={() => onDelete(id)}></button>
     </li>
   );
+};
+
+Task.propTypes = {
+  id: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  done: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
 export default Task;
